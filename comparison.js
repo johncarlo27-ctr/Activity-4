e26febb957602b1916b60850db2400be2235db37
unknown <johncarloaltura705@gmail.com>
2024-09-13 15:00:16 +0800

    Initial commit with comparison.js

---
+++ comparison.js
@@ -0,0 +1,41 @@
+let firstFavNum = 8;
+let secondFavNum = 27;
+
+//Greater than
+let greaterThan = firstFavNum > secondFavNum;
+console.log(greaterThan);
+
+//Less than
+let lessThan = firstFavNum < secondFavNum;
+console.log(lessThan);
+
+//Greater than or equal
+let greaterThanOrequal = firstFavNum >= secondFavNum;
+console.log(greaterThanOrequal);
+
+//Less than or equal to
+let lessThanOrEqualTo = firstFavNum <= secondFavNum;
+console.log(lessThanOrEqualTo);
+
+//Strictly equality
+let strictlyEquality = firstFavNum == secondFavNum;
+console.log(strictlyEquality);
+
+//Loose inequality
+let looseInequality = firstFavNum !== secondFavNum;
+console.log(looseInequality);
+
+//AND
+let and = firstFavNum && secondFavNum;
+console.log(and);
+
+//OR
+let or = firstFavNum === secondFavNum;
+console.log(or);
+
+//NOT
+let isCold = true;
+let isHot = false;
+
+console.log(!isCold);
+console.log(!isHot);
